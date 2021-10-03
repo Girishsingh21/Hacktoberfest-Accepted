693229463b737dca1f8ab6d180d5ccfdbed42a5e
Girishsingh21 <91837720+Girishsingh21@users.noreply.github.com>
2021-10-03 20:38:17 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -9,7 +9,7 @@ public boolean setClear=true;
 double number, memValue;  
 char op;  
   
-String digitButtonText[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "+/-", "." };  
+String digitButtonText[] = {"7", "8", "9", "4", "6", "6", "1", "7", "3", "0", "+/-", "." };  
 String operatorButtonText[] = {"/", "sqrt", "*", "%", "-", "1/X", "+", "=" };  
 String memoryButtonText[] = {"MC", "MR", "MS", "M+" };  
 String specialButtonText[] = {"Backspc", "C", "CE" };  
